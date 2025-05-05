c4702624b6b44477952a1ec419c8325afd0805f2
Itron_Al_Lenn <itronallenn@gmail.com>
2025-05-05 17:51:39 +0200

    feat(tooling/git-hooks): Add git hooks to install script

--- tools/install.sh
+++ tools/install.sh
@@ -2,7 +2,17 @@
 DIR=$(dirname "$0")
 TOOL_PATH="$(pwd)/$DIR/tools.py"
 
-chmod -v +x "$TOOL_PATH"
+echo "Link git hooks..."
+for hook in "$(pwd)"/.githooks/*; do
+  if [ -f "$hook" ]; then
+    hook_name=$(basename "$hook")
+    echo "Installing hook: $hook_name"
+    ln -sfv "$hook" "$(pwd)/.git/hooks/$hook_name"
+  fi
+done
+
+echo "Make Script executable..."
+chmod +x "$TOOL_PATH"
 
 mkdir -pv ~/.local/bin
 ln -sfv "$TOOL_PATH" ~/.local/bin/risc-tool
