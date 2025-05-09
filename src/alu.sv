6d59916124c639a00a63ad45239f61e1b9f89bb7
Itron_Al_Lenn <itronallenn@gmail.com>
2025-05-09 14:41:24 +0200

    feat(component/alu): Change how ALU works

--- src/alu.sv
+++ src/alu.sv
@@ -4,11 +4,14 @@ module alu (
     output logic [31:0] result_o,
     input logic [31:0] operand_1_i,
     input logic [31:0] operand_2_i,
-    input alu_operation_e operation_i
+    input alu_funct7_e funct7_i,
+    input alu_funct3_e funct3_i
 );
   always_comb begin
-    unique case (operation_i)
-      // Will be added when implementing the operations
+    case (funct7_i)
+    endcase
+
+    case (funct3_i)
     endcase
   end
 endmodule
