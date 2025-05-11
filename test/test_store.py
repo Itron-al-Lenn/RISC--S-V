59d131d5082f5b375d2db47bf71b021697f60ced
Itron_Al_Lenn <itronallenn@gmail.com>
2025-05-11 20:11:16 +0200

    feat(instuction/store-load): Add store instruction and fixes load instruction

--- test/test_store.py
+++ test/test_store.py
@@ -1,15 +1,15 @@
-from test.util import PROJ_PATH, check_ram, check_reg, run_till_end, runner
+from test.util import PROJ_PATH, check_ram, run_till_end, runner
 
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
 
-INSTRUCTION_FILE = PROJ_PATH / 'test' / 'instructions' / 'load.hex'
+INSTRUCTION_FILE = PROJ_PATH / 'test' / 'instructions' / 'store.hex'
 
 def test_cpu_shift():
     runner('cpu',
            instruction_file=INSTRUCTION_FILE,
-           test_module="test.test_load",
+           test_module="test.test_store",
 )
 
     
@@ -26,10 +26,9 @@ async def cpu(dut):
     dut.reset.value = 0
 
     await run_till_end(dut, INSTRUCTION_FILE)
-    
-    check_ram(dut, 0x00000200, 0xFF345678)
-    check_reg(dut, 2, 0xFF345678)
-    check_reg(dut, 3, 0x00003456)
-    check_reg(dut, 4, 0xFFFFFFFF)
-    check_reg(dut, 5, 0x00003456)
-    check_reg(dut, 6, 0x000000FF)
+        
+    check_ram(dut, 0x00000200, 0x12345678)
+    check_ram(dut, 0x00000204, 0x00005678)
+    check_ram(dut, 0x00000208, 0x00000078)
+    check_ram(dut, 0x0000020c, 0x34567878)
+    check_ram(dut, 0x00000210, 0x00000012)
